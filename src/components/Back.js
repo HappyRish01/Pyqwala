c0f7949bfb2ab58dc7f84da42eaeec2b5a253884
HappyRish01 <rishk219@gmail.com>
2024-06-05 19:01:29 +0530

    bcakground check

--- src/components/Back.js
+++ src/components/Back.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import "./Cssforback.css"
+import image from "./bg.jpg"
 // import bgimage from './bgimage.jpg';
 
 
 export default function Back() {
+  const mycss = {
+    backgroundImage:`url(${image})`,
+    backgroundSize: "cover",
+    backgroundRepeat: 'no-repeat',
+    height: '90vh'
+  }
+  
   var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
   console.log(is_mobile)
+  const bgLink = "https://firebasestorage.googleapis.com/v0/b/pyqs-4575a.appspot.com/o/assets%2FWhatsApp%20Image%202024-06-05%20at%2018.24.46_f2176783.jpg?alt=media&token=7fc1a2fd-2cce-48de-a7a9-56694aab7758";
   return (
-
-    <>
-
-
-  
-  <div className='bg'>
-    <img src='https://www.paraggupta.com/wp-content/uploads/20220402152158CUET-.jpg' alt='Backgroundimage' id="bgimage"/>
-            </div>
-    </>
+  // <img src={bgLink} alt="background" />
+  <div style={mycss}></div>
   )
 }
