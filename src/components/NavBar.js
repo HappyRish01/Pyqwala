968e6717978efa3f226c5d0b03c698e52ba6efff
ARCANE <149970860+Arcane-stack@users.noreply.github.com>
2024-06-05 16:14:30 +0530

    Frontend

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+// import PropTypes from 'prop-types'
+// import react, { Component } from 'react'
+// import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react'
+import './Csss2.css';
+
+export default function NavBar() {
+  return (
+    <div >
+      <nav className="navbar text-center">
+      <div className="navbar__brand text-center ">PyqWalaDost</div>
+      <ul className="navbar__menu">
+
+      </ul>
+    </nav>
+
+    </div>
+  )
+}
+
+
+
